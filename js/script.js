3b7c5012cd4b02c0cabd270ac57eec624218bd4e
rikke-dishington <85433495+rikke-dishington@users.noreply.github.com>
2022-04-06 20:39:45 +0200

    Answers to questions 1-8

---
+++ js/script.js
@@ -0,0 +1,80 @@
+const cats = [
+  {
+    name: "Blob",
+    age: 10,
+  },
+  {
+    name: "Harold",
+  },
+  {
+    name: "Blurt",
+    age: 21,
+  },
+];
+
+// Question 1
+const cat = {
+  complain: function () {
+    console.log("Meow!");
+  },
+};
+
+cat.complain();
+
+// Question 2
+const heading = document.querySelector("h3");
+
+heading.innerHTML = "Updated heading";
+
+// Question 3
+heading.style.fontSize = "2em";
+
+// Question 4
+heading.classList.add("subheading");
+
+// Question 5
+const paragraphs = document.querySelectorAll("p");
+
+for (let i = 0; i < paragraphs.length; i++) {
+  paragraphs[i].style.color = "red";
+}
+
+// Question 6
+const resultsContainer = document.querySelector(".results");
+
+resultsContainer.innerHTML += "<p>New paragraph</p>";
+resultsContainer.style.backgroundColor = "yellow";
+
+// Question 7
+function catsName(list) {
+  for (let i = 0; i < list.length; i++) {
+    console.log(list[i].name);
+  }
+}
+
+catsName(cats);
+
+// Question 8
+function createCats(cats) {
+  let html = "";
+
+  for (let i = 0; i < cats.length; i++) {
+    let catsAge = "Age unknown";
+
+    if (cats[i].age) {
+      catsAge = cats[i].age;
+    }
+
+    html += `<div>
+                <h5>${cats[i].name}</h5>
+                <p>${catsAge}</p>
+            </div>`;
+  }
+
+  return html;
+}
+
+const newHTML = createCats(cats);
+
+const catContainer = document.querySelector(".cat-container");
+catContainer.innerHTML = newHTML;
